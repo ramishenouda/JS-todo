3344f3220b2814b4c75bc45617d30b02f9ad17be
Rami Shenouda <ramishenouda@outlook.com>
2019-12-15 15:39:45 +0200

    fetching data done

--- script.js
+++ script.js
@@ -1,18 +1,48 @@
-let todos = document.getElementById("todos");
-
-let requestURL = 'https://github.com/ramishenouda/JS-todo/blob/master/data.json';
-let request = new XMLHttpRequest();
-
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
-
-request.onload = function() {
-    const todoItems = request.response;
-    showTodos(todoItems);
+let Todos = document.getElementById("todos");
+let todos = {
+    day1: {
+        history: "15/12",
+        done: false,
+        todo: [
+            {
+                Syntax : [
+                    "- Lecutre 4",
+                    "- Quick revision Lecture 1, 2"
+                ],
+                Complexity : [
+                    "- 1/2 First midterm"
+                ],
+                Other : [
+                    "CreatingTodoList"
+                ]
+            }
+        ]
+    }
 }
 
-function showTodos(todosJSONObject)
-{
-    console.log(todosJSONObject.day1);
+function showTodos() {
+    Object.keys(todos).forEach(key => {
+        let day = todos[key];
+        let dayTodos = day["todo"];
+
+        Todos.innerHTML += `<span id='task'> ${day["history"]} <br> </span>`;
+
+        Object.keys(dayTodos).forEach(todo => {  
+            Object.keys(dayTodos).forEach(subject => {                
+                let tasks = dayTodos[subject];                
+                Object.keys(tasks).forEach(task => {
+                    Todos.innerHTML += `<span id='task'>${task} </span>`;
+                    Todos.innerHTML += "<ol>";
+                    for(let i = 0; i < tasks[task].length; i++) {
+                        Todos.innerHTML += `<li> ${tasks[task][i]} </li>`;
+                    }
+                    Todos.innerHTML += "</ol>";
+                });              
+            });
+        });
+
+        Todos.innerHTML += "<br><hr><br>";
+    });
 }
+
+showTodos()
